72c02f73e9e181f76aa165f523294bdcc1c74459
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-13 17:32:33 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+ num = num1/num2
+ num
 end
 
 def assign_variable(value)
-  
+  value
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
+def greeting(greeting,name)
+  concat = greeting + name
+  concat
 
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  expert = "expert"
+  expert
 end
 
-def pizza_party
+def pizza_party(pizza = "cheese")
+  pizza
 
 end
